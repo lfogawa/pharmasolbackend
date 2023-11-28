d50e26ecded5cbf498a9ea58d36c91a4a6821c4e
lfogawa <lfogawa0@gmail.com>
2023-11-28 11:21:34 -0300

    initial commit

---
+++ src/main/java/com/devinhouse/pharmasol/PharmasolApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pharmasol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmasolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmasolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/pharmasol/PharmasolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pharmasol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmasolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
