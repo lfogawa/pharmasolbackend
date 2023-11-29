65517cd284e594f58dfed3ea6d936cd0193cc65c
lfogawa <lfogawa0@gmail.com>
2023-11-29 11:34:36 -0300

    import org.hibernate.query.Page adjusted to import org.springframework.data.domain.Page

--- src/main/java/com/devinhouse/pharmasol/service/PharmacyService.java
+++ src/main/java/com/devinhouse/pharmasol/service/PharmacyService.java
@@ -3,7 +3,7 @@ package com.devinhouse.pharmasol.service;
 import com.devinhouse.pharmasol.dtos.PharmacyResponse;
 import com.devinhouse.pharmasol.model.Pharmacy;
 import com.devinhouse.pharmasol.repository.PharmacyRepository;
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
