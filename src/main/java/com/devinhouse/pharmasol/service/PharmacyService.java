3f68701f0e00218de78fc7a22d2b7280a36d08a1
lfogawa <lfogawa0@gmail.com>
2023-11-29 11:20:09 -0300

    listAll added to pharmacy service

--- src/main/java/com/devinhouse/pharmasol/service/PharmacyService.java
+++ src/main/java/com/devinhouse/pharmasol/service/PharmacyService.java
@@ -2,7 +2,9 @@ package com.devinhouse.pharmasol.service;
 
 import com.devinhouse.pharmasol.model.Pharmacy;
 import com.devinhouse.pharmasol.repository.PharmacyRepository;
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,10 @@ public class PharmacyService {
 
     public void save(Pharmacy pharmacy){
         pharmacyRepository.save(pharmacy);
+    }
+
+    public Page<PharmacyResponse> listAll(Pageable pageable){
+        return this.pharmacyRepository.findAll(pageable).map(PharmacyResponse::new);
     }
 
 
